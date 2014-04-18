e03ee4b9b478c0e1814e4ecb06ff06863bc4ff00
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-18 06:12:14 +0000

    reverting v8 to 3.21.17 and simplifying build
    - examples all work again
    - turning off in18 support
    - pulling down gyp manually instead of via `make dependencies`
    - forcing clang++ for project compilation to avoid ambiguities that g++
      encounters

--- .common.mk
+++ .common.mk
@@ -1,16 +1,12 @@
 # linked to from dir one level below
 include ../v8.mk
 
-CXX ?= g++
-
+# force clang++ since g++ thinks certain things like FunctionCallbackInfo are ambiguous whereas clang++ resolves them just fine
 build: 
-	$(CXX) -I$(V8)/include main.cc -o main                         \
+	clang++ -I$(V8)/include main.cc -o main                         \
 	-Wl,--start-group                                              \
-		$(V8)/out/$(V8_ARCH)/obj.target/third_party/icu/libicudata.a \
 		$(V8)/out/$(V8_ARCH)/obj.target/tools/gyp/libv8_base.x64.a   \
-		$(V8)/out/$(V8_ARCH)/obj.target/third_party/icu/libicui18n.a \
 		$(V8)/out/$(V8_ARCH)/obj.target/tools/gyp/libv8_snapshot.a   \
-		$(V8)/out/$(V8_ARCH)/obj.target/third_party/icu/libicuuc.a   \
 	-Wl,--end-group                                                \
 	-lrt
 
--- v8.mk
+++ v8.mk
@@ -3,24 +3,26 @@ LINK        = g++
 GYP_DEFINES = 
 
 ROOT	  = $(dir $(lastword $(MAKEFILE_LIST)))
-V8      = $(ROOT)deps/v8
-V8_REPO = $(V8)/.gitignore
-V8_LIB  = $(V8)/out/native/obj.target/tools/gyp/libv8_{base.x64,snapshot}.a
-V8_GYP  = $(V8)/build/gyp/gyp
+V8      = $(ROOT)deps/v8/
+V8_REPO = $(V8).gitignore
+V8_LIB  = $(V8)out/native/obj.target/tools/gyp/libv8_{base.x64,snapshot}.a
+V8_GYP  = $(V8)build/gyp/gyp
 V8_ARCH ?= native
+V8_VERSION ?= 3.21.17
 
 v8: $(V8_LIB)
 	
 $(V8_LIB): $(V8_REPO) $(V8_GYP)
 	cd $(V8)  && \
-	CXX=$(CXX) LINK=$(LINK) GYP_DEFINES=$(GYP_DEFINES) $(MAKE) $(V8_ARCH) -j4 console=readline
+	CXX=$(CXX) LINK=$(LINK) GYP_DEFINES=$(GYP_DEFINES) $(MAKE) $(V8_ARCH) -j4 console=readline library=shared snapshot=on i18support=off
 
 $(V8_REPO):
 	cd $(ROOT)deps && \
-	curl -L https://github.com/v8/v8/archive/3.26.19.tar.gz | tar xvzf - && \
-	mv v8-3.26.19 v8
+	curl -L https://github.com/v8/v8/archive/$(V8_VERSION).tar.gz | tar xvzf - && \
+	mv v8-$(V8_VERSION) v8
 
+# instructions say to `make dependencies`, but all we need is gyp
 $(V8_GYP):
-	cd $(V8) && CXX=$(CXX) LINK=$(LINK) GYP_DEFINES=$(GYP_DEFINES) $(MAKE) dependencies
+	test -d $(V8)build/gyp || (mkdir -p $(V8)build && git clone https://git.chromium.org/external/gyp.git $(V8)build/gyp)
 
 .PHONY: v8
