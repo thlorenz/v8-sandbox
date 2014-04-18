e0ad1c20988c6b785b268dc6a2c14b271e2b0aa1
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-18 04:07:49 +0000

    trying to speed up v8 build

--- v8.mk
+++ v8.mk
@@ -1,24 +1,30 @@
-CXX ?= clang++
+CXX         = g++
+LINK        = g++
+GYP_DEFINES = 
 
 ROOT	  = $(dir $(lastword $(MAKEFILE_LIST)))
 V8      = $(ROOT)/deps/v8
 V8_REPO = $(V8)/.gitignore
-V8_LIB  = $(V8)/out/native/libv8_{base.x64,snapshot}.a
+V8_LIB  = $(V8)/out/native/obj.target/tools/gyp/libv8_{base.x64,snapshot}.a
 V8_GYP  = $(V8)/build/gyp/gyp
 
 v8: $(V8_LIB)
 	
 $(V8_LIB): $(V8_REPO) $(V8_GYP)
-	cd ./deps/v8 && \
-	CXX=$(CXX) werror=no $(MAKE) native -j8 library=shared snapshot=on console=readline i18nsupport=off
+	cd $(V8) && \
+	CXX=$(CXX) LINK=$(LINK) GYP_DEFINES=$(GYP_DEFINES) werror=no $(MAKE) native -j4 library=shared snapshot=on console=readline 
+	#i18nsupport=off
 
 $(V8_REPO):
-	cd deps && curl -L https://github.com/v8/v8/archive/3.24.35.28.tar.gz | tar xvzf - &&\
-	mv v8-3.24.35.28 v8
+	cd $(ROOT)/deps &&\
+		curl -L https://github.com/v8/v8/archive/3.26.19.tar.gz | tar xvzf - && \
+		mv v8-3.26.19 v8
 
 # `make dependencies` pulls in ICU, but all we need is gyp
 $(V8_GYP):
-	cd ./deps/v8 && \
-	test -d ./build/gyp || (mkdir -p ./build && git clone https://git.chromium.org/external/gyp.git ./build/gyp)
+	cd $(V8) && \
+	CXX=$(CXX) LINK=$(LINK) GYP_DEFINES=$(GYP_DEFINES) werror=no $(MAKE) dependencies
+	#test -d ./build/gyp || (mkdir -p ./build && git clone https://git.chromium.org/external/gyp.git ./build/gyp)
+
 
 .PHONY: v8
