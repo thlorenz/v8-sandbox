aef53f73f5d79e4bd3d245db1f15cf418c2ac677
Thorsten Lorenz <thlorenz@gmx.de>
2015-07-17 15:47:11 -0400

    001: updating getting started to run with v8 v.3 - basically copied example from: https://developers.google.com/v8/get_started

--- 001-getting-started/main.cc
+++ 001-getting-started/main.cc
@@ -1,19 +1,47 @@
-#include <v8.h>
+#include "include/v8.h"
+#include "libplatform/libplatform.h"
 
 using namespace v8;
 
-int main(int argc, const char *argv[]) {
-  Isolate* isolate = Isolate::GetCurrent();
+int main(int argc, char* argv[]) {
+  // Initialize V8.
+  V8::InitializeICU();
+  Platform* platform = platform::CreateDefaultPlatform();
+  V8::InitializePlatform(platform);
+  V8::Initialize();
 
-  HandleScope handle_scope(isolate);
-  Handle<Context> context = Context::New(isolate);
-  Context::Scope context_scope(context);
+  // Create a new Isolate and make it the current one.
+  Isolate* isolate = Isolate::New();
+  {
+    Isolate::Scope isolate_scope(isolate);
 
-  Handle<String> source = String::NewFromUtf8(isolate, "'Hello, World! ' + (2000 + 14)");
-  Handle<Script> script = Script::Compile(source);
-  Handle<Value> result  = script->Run();
+    // Create a stack-allocated handle scope.
+    HandleScope handle_scope(isolate);
 
-  String::Utf8Value utf8(result);
-  printf("%s\n", *utf8);
+    // Create a new context.
+    Local<Context> context = Context::New(isolate);
+
+    // Enter the context for compiling and running the hello world script.
+    Context::Scope context_scope(context);
+
+    // Create a string containing the JavaScript source code.
+    Local<String> source = String::NewFromUtf8(isolate, "'Hello' + ', World!'");
+
+    // Compile the source code.
+    Local<Script> script = Script::Compile(source);
+
+    // Run the script to get the result.
+    Local<Value> result = script->Run();
+
+    // Convert the result to an UTF8 string and print it.
+    String::Utf8Value utf8(result);
+    fprintf(stderr, "%s\n", *utf8);
+  }
+
+  // Dispose the isolate and tear down V8.
+  isolate->Dispose();
+  V8::Dispose();
+  V8::ShutdownPlatform();
+  delete platform;
   return 0;
 }
